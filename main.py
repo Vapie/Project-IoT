4d42a1000fc7a39c3c8052c42b1f7886c0c371ca
Vapie <48350646+Vapie@users.noreply.github.com>
2020-09-24 18:21:22 +0200

    First commit from home

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 import network
 import time
+import utime
+from pysense import Pysense
+from MPL3115A2 import MPL3115A2, ALTITUDE
+from machine import RTC
+
+rtc = RTC()
+print(rtc.now())
 # setup as a station
+"""
 print("yo")
 wlan = network.WLAN(mode=network.WLAN.STA)
 wlan.connect('We need the Fi', auth=(network.WLAN.WPA2, 'ricardoo'))
 while not wlan.isconnected():
     time.sleep_ms(50)
 print(wlan.ifconfig())
+"""
+
+"""
+voila comment récupérer l'altitude
+py = Pysense()
+mp = MPL3115A2(py, mode=ALTITUDE)
+
+for i in range(100):
+    print(str(now())+ str(mp.altitude()))
+    utime.sleep(1)
+"""
+
+#test connection
+#une connexion aété effextuée :
+
+
+#si connecté
+
+#si pas connecté
+
 
 # now use socket as usual
