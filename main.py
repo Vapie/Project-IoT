082214baacf760efedaad4608c0aa667c1451fe1
Vapie <48350646+Vapie@users.noreply.github.com>
2020-09-24 22:43:04 +0200

    commit last propgram error

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-import network
+from network import WLAN
 import time
 import utime
+import pycom
 from pysense import Pysense
 from MPL3115A2 import MPL3115A2, ALTITUDE
 from machine import RTC
+import machine
 
+wlan = WLAN(mode=WLAN.STA)
+
+nets = wlan.scan()
+for net in nets:
+    if net.ssid == 'aaaa':
+        print('Network found!')
+        wlan.connect(net.ssid, auth=(net.sec, 'yoyoyoyo'), timeout=5000)
+        while not wlan.isconnected():
+            machine.idle() # save power while waiting
+        print('WLAN connection succeeded!')
+        break
+"""
 rtc = RTC()
 print(rtc.now())
+rtc.ntp_sync("https://www.impots.gouv.fr/portail/")
+print(rtc.now())
+#connexion au serveur pour mise a jour de l'heure
+"""
+"""
+rtc.ntp_sync("pool.ntp.org")
+
+rtc = RTC()
+rtc.init((2014, 5, 1, 4, 13, 0, 0, 0))
+print(rtc.now())
+
+print(rtc.now())
 # setup as a station
+"""
+
 """
 print("yo")
 wlan = network.WLAN(mode=network.WLAN.STA)
@@ -28,7 +56,7 @@ for i in range(100):
 """
 
 #test connection
-#une connexion aété effextuée :
+# une connexion aété effextuée :
 
 
 #si connecté
