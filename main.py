3891829970200810fb92ef713d5950b3cf1dd4e2
Vapie <48350646+Vapie@users.noreply.github.com>
2020-09-27 22:23:35 +0200

    heure qui marche + altitude , a fgaire :     -histo +rétro datage     -try connec puis     -envoi base

--- main.py
+++ main.py
@@ -6,23 +6,40 @@ from pysense import Pysense
 from MPL3115A2 import MPL3115A2, ALTITUDE
 from machine import RTC
 import machine
+import ntptime
 
 wlan = WLAN(mode=WLAN.STA)
 
+print("start")
 nets = wlan.scan()
+print("les nets: " + str(nets))
 for net in nets:
-    if net.ssid == 'aaaa':
+    if net.ssid == 'Livebox-fd84':
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'yoyoyoyo'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, 'EDCE75E392ECDDD3F25ECCF7C4'), timeout=5000)
         while not wlan.isconnected():
-            machine.idle() # save power while waiting
+            utime.sleep(3)
         print('WLAN connection succeeded!')
         break
+rtc = RTC()
+t = ntptime.time()
+tm = utime.gmtime(t)
+rtc.init((tm[0]+30, tm[1], tm[2], tm[6] , tm[3]+2 , tm[4], tm[5], 0))
+
+py = Pysense()
+mp = MPL3115A2(py, mode=ALTITUDE)
+
+r = rtc.now()
+print("le "+str(r[2])+"/"+str(r[1])+"/"+str(r[0]) +" a " + str(r[4]) +"h"+str(r[5])+" a l'altitude " + str(mp.altitude()))
+
+
 """
 rtc = RTC()
 print(rtc.now())
 rtc.ntp_sync("https://www.impots.gouv.fr/portail/")
 print(rtc.now())
+
+wlan.disconnect()
 #connexion au serveur pour mise a jour de l'heure
 """
 """
